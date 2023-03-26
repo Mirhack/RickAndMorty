45676632381f7fd40791716c05c12b1c2e8fc619
Mirhack <mirhack8@gmail.com>
2023-03-26 15:00:36 +0300

    Logging interceptor, Room dependency

--- app/src/main/java/com/mirhack/rickandmorty/di/RetrofitModule.kt
+++ app/src/main/java/com/mirhack/rickandmorty/di/RetrofitModule.kt
@@ -8,6 +8,9 @@ import dagger.hilt.components.SingletonComponent
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 import javax.inject.Singleton
+import com.mirhack.rickandmorty.BuildConfig
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
 
 @Module
 @InstallIn(SingletonComponent::class)
@@ -21,11 +24,27 @@ class RetrofitModule {
 
     @Provides
     @Singleton
-    fun provideRetrofit(): Retrofit {
+    fun provideRetrofit(okHttpClient: OkHttpClient): Retrofit {
         return Retrofit.Builder()
             .baseUrl(BASE_URL)
+            .client(okHttpClient)
             .addConverterFactory(GsonConverterFactory.create())
             .build()
+    }
+
+    @Provides
+    @Singleton
+    fun provideOkHttpClient(loggingInterceptor: HttpLoggingInterceptor): OkHttpClient {
+        return OkHttpClient.Builder()
+            .addInterceptor(loggingInterceptor)
+            .build()
+    }
+
+    @Provides
+    fun httpLoggingInterceptor(): HttpLoggingInterceptor {
+        val logger = HttpLoggingInterceptor()
+        if (BuildConfig.DEBUG) logger.level = HttpLoggingInterceptor.Level.BASIC
+        return logger
     }
 
     companion object {
